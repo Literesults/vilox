37f7cbf59e21db9a33b229e1b87ff3e998bbe007
ebuberoderick <ebuberoderick2@gmail.com>
2024-08-16 15:27:50 +0100

    removed landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,7 @@
-import Hero from "./components/molecules/Hero.js";
-import About from "./components/molecules/About.js";
-import Contact from "./components/molecules/Contact.js";
-import Download from "./components/molecules/Download.js";
-import Faq from "./components/molecules/Faq.js";
-import Footer from "./components/molecules/Footer.js";
-import Navbar from "./components/navbar/Navbar.js";
-import WhyUs from "./components/molecules/why.js";
+"use client"
+import { useRouter } from "next/navigation.js";
 
 export default function Home() {
- return(
-    <>
-    <Navbar />
-    <Hero />
-    <WhyUs />
-    <About />
-   <Faq /> 
-   <Download />   
-   <Contact />
-   <Footer />
-    </>
- )
-
+   const router = useRouter()
+   router.push("/admin/dashboard")
 }
